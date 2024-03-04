21732192a006d3c8ef08e85f9b1084ecd4182d27
AcePatterson <acepttrsn@gmail.com>
2024-03-04 06:50:02 -0800

    feat:forget_password

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -1,21 +1,14 @@
 "use client";
 
 import React, { useState, FormEvent } from "react";
-import { useRouter } from "next/navigation";
-import { Flip, ToastContainer, toast } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
 
 interface FormData {
   email: string;
-  password: string;
 }
 
-const Login = () => {
-  const router = useRouter();
-  const notify = () => toast("You have successfully signed up!");
+const reset_password = () => {
   const [formData, setFormData] = useState<FormData>({
     email: "",
-    password: "",
   });
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -24,7 +17,7 @@ const Login = () => {
 
   async function sendDataToServer(e: FormEvent) {
     e.preventDefault(); // Prevent default form submission behavior
-    const url = "http://localhost:5000/api/login"; // Replace with your domain in production
+    const url = "http://localhost:5000/api/reset_password"; // Replace with your domain in production
     try {
       // Send a POST request
       const response = await fetch(url, {
@@ -42,7 +35,6 @@ const Login = () => {
       const result = await response.json();
       console.log("Response from the server:", result);
       // Assuming you would navigate after successful login
-      notify();
       return result;
     } catch (error) {
       if (error instanceof Error) {
@@ -51,7 +43,6 @@ const Login = () => {
       // Handle errors here or rethrow them depending on your needs
     }
   }
-
   return (
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
@@ -62,7 +53,7 @@ const Login = () => {
             alt="Your Company"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to your account
+            Forgot password?
           </h2>
         </div>
 
@@ -81,37 +72,9 @@ const Login = () => {
                   name="email"
                   type="email"
                   value={formData.email}
-                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => onChange(e)}
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Password
-                </label>
-                <div className="text-sm">
-                  <a
-                    href="/reset-password"
-                    className="font-semibold text-indigo-600 hover:text-indigo-500"
-                  >
-                    Forgot password?
-                  </a>
-                </div>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  value={formData.password}
-                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => onChange(e)}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    onChange(e)
+                  }
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
@@ -123,37 +86,14 @@ const Login = () => {
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Sign in
+                Send
               </button>
             </div>
           </form>
-
-          <p className="mt-10 text-center text-sm text-gray-500">
-            Not a member?{" "}
-            <button
-              type="button"
-              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              onClick={() => router.push("/register")}
-            >
-              Sign up
-            </button>
-          </p>
         </div>
-        <ToastContainer 
-          position="top-right"
-          autoClose={2000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="light"
-          transition={Flip} />
       </div>
     </>
   );
 };
 
-export default Login;
+export default reset_password;
